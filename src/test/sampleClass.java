c357e522869c5d4ccedb6024a97c304efe4f5295
NehaGoelMittal <neha.goel02@gmial.com>
2022-09-17 13:45:27 +0530

    Third command

--- src/test/sampleClass.java
+++ src/test/sampleClass.java
@@ -5,6 +5,9 @@ public class sampleClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Test");
+		sampleClass s = new sampleClass();
+		System.out.println(s.getClass());
+		
 		// TODO Auto-generated method stub
 
 	}
